a72e16367ffb67ac56763c384a715109326b8c05
Chris Maguire <cwmaguire@gmail.com>
2014-02-19 19:32:30 -0800

    Rename TeensyKey sketch: conflicts with OSX build.
    I tried building this on a Mac today (10.9) and the TeensyKey directory
    conflicted with the executable name since OSX picky about case.

--- TeensyKeySketch/TeensyKeySketch.ino
+++ TeensyKeySketch/TeensyKeySketch.ino
@@ -1,7 +1,7 @@
- /* TeensyKey
- 
+/* TeensyKey
+
    TeensyKey receives an int from a dedicated program
-   running on a Linux or Mac machine and returns a 
+   running on a Linux or Mac machine and returns a
    password belonging to that int.
 
    You must select Raw HID from the "Tools > USB Type" menu
@@ -20,16 +20,21 @@ byte buffer[64];
 unsigned int waitTimeout = 10000;
 unsigned int packetCount = 0;
 
-const char passwords[][64] = {"one", "two", "three"};
+const char passwords[][64] = {
+"Password1",
+"1234",
+"TOP SECRET",
+"Samsung1"
+}
 
 void loop() {
   int n, password_index;
   int password_char_index = 0;
-  
+
   Serial.println(F("Waiting for password index ..."));
   n = RawHID.recv(buffer, waitTimeout);
   if (n > 0) {
-    // the computer sent a message. 
+    // the computer sent a message.
     Serial.print(F("Received packet, first byte: "));
     Serial.println((int)buffer[0]);
 
@@ -41,9 +46,9 @@ void loop() {
       Serial.println((char) buffer[password_char_index + 1]);
       password_char_index += 1;
     }
-    
+
     buffer[password_char_index + 1] = 0;
-    
+
     // put the length of the password, including \0 in the first byte
     buffer[0] = password_char_index;
 
@@ -51,7 +56,7 @@ void loop() {
     for (int i = password_char_index + 1; i < 64 - password_char_index; i++) {
       buffer[i] = 0;
     }
-    
+
     // actually send the packet
     n = RawHID.send(buffer, 100);
     if (n > 0) {
